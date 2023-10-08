a36a87f3587df4005eaa84fad1f3d812288415c7
Silver Dime <77194774+Shrey-Dubey-25@users.noreply.github.com>
2023-10-08 17:47:07 +0530

    Added Solution - GfG to GitHub

---
+++ Easy/Array Subset of another array/array-subset-of-another-array.cpp
@@ -0,0 +1,57 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+string isSubset(int a1[], int a2[], int n, int m) ;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n, m;
+        cin >> n >> m;
+        int a1[n], a2[m];
+
+        for (int i = 0; i < n; i++) {
+            cin >> a1[i];
+        }
+        for (int i = 0; i < m; i++) {
+            cin >> a2[i];
+        }
+
+        cout << isSubset(a1, a2, n, m) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
+string isSubset(int a1[], int a2[], int n, int m) {
+    int max=INT_MIN;
+    if(m==0){
+        return "Yes";
+    }
+    if(m>n){
+        return "No";
+    }
+    for(int i=0;i<m;i++){
+        if(a2[i]>max){
+            max=a2[i];
+        }
+    }
+    vector<int>h(max+1,0);
+    for(int i=0;i<m;i++){
+        h[a2[i]]++;
+    }
+    for(int i=0;i<n;i++){
+        if(a1[i]>=0&&a1[i]<=max){
+            h[a1[i]]--;
+        }
+    }
+    for(auto i:h){
+        if(i>0){
+            return "No";
+        }
+    }
+    return "Yes";
+}
