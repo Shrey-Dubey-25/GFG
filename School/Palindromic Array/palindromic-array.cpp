13b52e9be51ab2a9572c23fb2d9ae698d0a407df
Silver Dime <77194774+Shrey-Dubey-25@users.noreply.github.com>
2023-10-08 23:44:50 +0530

    Added Solution - GfG to GitHub

---
+++ School/Palindromic Array/palindromic-array.cpp
@@ -0,0 +1,52 @@
+//{ Driver Code Starts
+#include<iostream>
+#include<string.h>
+using namespace std;
+
+
+// } Driver Code Ends
+/*Complete the function below*/
+
+class Solution {
+public:
+    bool ispali(int num,int n){
+        string s=to_string(num);
+        int i=0,j=s.length()-1;
+        while(i<j){
+            if(s[i]!=s[j]){
+                return false;
+            }
+            i++;
+            j--;
+        }
+        return true;
+    }
+
+    int PalinArray(int a[], int n){
+        for(int i=0;i<n;i++){
+            if(!ispali(a[i],n)){
+                return 0;
+            }
+        }
+        return 1;
+    }
+};
+
+//{ Driver Code Starts.
+
+int main()
+{
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		int a[n];
+		for(int i = 0; i < n; i++)
+			cin>>a[i];
+		Solution obj;
+		cout<<obj.PalinArray(a,n)<<endl;
+	}
+}
+// } Driver Code Ends
